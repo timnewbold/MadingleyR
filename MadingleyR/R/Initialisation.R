72dd0688e00b6d9bcdfd9e02d6b5ff5582f550e6
Tim Newbold <timnewbold.research@gmail.com>
2016-11-15 11:27:21 +0000

    Added model initialisation to MadingleyR

--- MadingleyR/R/Initialisation.R
+++ MadingleyR/R/Initialisation.R
@@ -1,36 +1,68 @@
-MadingleyInit <- function(settings){
+MadingleyInitialisation <- function(bottom_lat=-65,
+                          top_lat=65,
+                          left_long=-180,
+                          right_long=180,
+                          label="MadingleyOutputs",
+                          cellsize=1,
+                          timestep="month",
+                          duration=100,
+                          burnin=0,
+                          impact="None",
+                          recovery=0,
+                          parallel_cells="yes",
+                          parallel_simulations="no",
+                          single_realm="",
+                          random="yes",
+                          extinction_threshold=1,
+                          max_cohorts=1000,
+                          track_processes="no",
+                          track_crosscell_processes="no",
+                          track_global_processes="no",
+                          output_detail="medium",
+                          output_metrics="no",
+                          track_land_use="no",
+                          live_outputs="no",
+                          specific_locations=list(latitudes=2,
+                                                  longitudes=33),
+                          save_final_state=FALSE,
+                          read_state=FALSE){
   
-  init <- list(bottom_lat=-65,
-               top_lat=65,
-               left_long=-180,
-               right_long=180,
-               label="MadingleyOutputs",
-               cellsize=1,
-               timestep="month",
-               duration=100,
-               burnin=0,
-               impact=None,
-               recovery=0,
-               parallel_cells="yes",
-               parallel_simulations="no",
-               single_realm="",
-               random="yes",
-               extinction_threshold=1,
-               max_cohorts=1000,
-               track_processes="no",
-               track_crosscell_processes="no",
-               track_global_processes="no",
-               output_detail="medium",
-               output_metrics="no",
-               track_land_use="no",
-               live_outputs="no",
-               specific_locations=False,
-               save_final_state=False,
-               read_state=False)
+  init <- list(bottom_lat=bottom_lat,
+               top_lat=top_lat,
+               left_long=left_long,
+               right_long=right_long,
+               label=label,
+               cellsize=cellsize,
+               timestep=timestep,
+               duration=duration,
+               burnin=burnin,
+               impact=impact,
+               recovery=recovery,
+               parallel_cells=parallel_cells,
+               parallel_simulations=parallel_simulations,
+               single_realm=single_realm,
+               random=random,
+               extinction_threshold=extinction_threshold,
+               max_cohorts=max_cohorts,
+               track_processes=track_processes,
+               track_crosscell_processes=track_crosscell_processes,
+               track_global_processes=track_global_processes,
+               output_detail=output_detail,
+               output_metrics=output_metrics,
+               track_land_use=track_land_use,
+               live_outputs=live_outputs,
+               specific_locations=ifelse(is.null(locations),FALSE,TRUE),
+               save_final_state=save_final_state,
+               read_state=read_state)
   
-  for (s in settings) {
-    init[names(x)] <- x
+  if (is.null(specific_locations)){
+    locations <- NULL
+  } else {
+    locations <- data.frame(Latitude=specific_locations$latitudes,
+                            Longitude=specific_locations$longitudes)
   }
+  
+  return(list(init=init,locations=locations))
   
 }
 
--- MadingleyR/R/Run.R
+++ MadingleyR/R/Run.R
@@ -1,8 +1,147 @@
-RunMadingley <- function(codeDir){
+RunMadingley <- function(codeDir,init){
   
   origDir <- getwd()
   
   setwd(paste(codeDir,"/MadingleyBuild",sep=""))
+  
+  initFileLookup <- list(bottom_lat=1,
+                         top_lat=1,
+                         left_long=1,
+                         right_long=1,
+                         label=4,
+                         cellsize=1,
+                         timestep=1,
+                         duration=1,
+                         burnin=1,
+                         impact=1,
+                         recovery=1,
+                         parallel_cells=1,
+                         parallel_simulations=1,
+                         single_realm=1,
+                         random=1,
+                         extinction_threshold=1,
+                         max_cohorts=1,
+                         track_processes=2,
+                         track_crosscell_processes=2,
+                         track_global_processes=2,
+                         output_detail=2,
+                         output_metrics=2,
+                         track_land_use=2,
+                         live_outputs=2,
+                         specific_locations=1,
+                         save_final_state=2,
+                         read_state=1)
+  
+  initDF <- data.frame(Parameter=NA,Value=unlist(init$init))
+  initDF$Value <- paste(initDF$Value)
+  initDF$Parameter[(row.names(initDF)=="bottom_lat")] <- "Bottom Latitude"
+  initDF$Parameter[(row.names(initDF)=="top_lat")] <- "Top Latitude"
+  initDF$Parameter[(row.names(initDF)=="left_long")] <- "Leftmost Longitude"
+  initDF$Parameter[(row.names(initDF)=="right_long")] <- "Rightmost Longitude"
+  initDF$Parameter[(row.names(initDF)=="label")] <- "Label"
+  initDF$Parameter[(row.names(initDF)=="cellsize")] <- "Grid Cell Size"
+  initDF$Parameter[(row.names(initDF)=="timestep")] <- "Timestep Units"
+  initDF$Parameter[(row.names(initDF)=="duration")] <- "Length of simulation (years)"
+  initDF$Parameter[(row.names(initDF)=="burnin")] <- "Burn-in (years)"
+  initDF$Parameter[(row.names(initDF)=="impact")] <- "Impact duration (years)"
+  initDF$Parameter[(row.names(initDF)=="recovery")] <- "Recovery duration (years)"
+  initDF$Parameter[(row.names(initDF)=="parallel_cells")] <- "Run Cells In Parallel"
+  initDF$Parameter[(row.names(initDF)=="parallel_simulations")] <- "Run Simulations In Parallel"
+  initDF$Parameter[(row.names(initDF)=="single_realm")] <- "Run Single Realm"
+  initDF$Parameter[(row.names(initDF)=="random")] <- "Draw Randomly"
+  initDF$Parameter[(row.names(initDF)=="extinction_threshold")] <- "Extinction Threshold"
+  initDF$Parameter[(row.names(initDF)=="max_cohorts")] <- "Maximum Number Of Cohorts"
+  initDF$Parameter[(row.names(initDF)=="track_processes")] <- "Track Processes"
+  initDF$Parameter[(row.names(initDF)=="track_crosscell_processes")] <- "Track Cross Cell Processes"
+  initDF$Parameter[(row.names(initDF)=="track_global_processes")] <- "Track Global Processes"
+  initDF$Parameter[(row.names(initDF)=="output_detail")] <- "Output Detail"
+  initDF$Parameter[(row.names(initDF)=="output_metrics")] <- "Output metrics"
+  initDF$Parameter[(row.names(initDF)=="track_land_use")] <- "Track land use specifics"
+  initDF$Parameter[(row.names(initDF)=="live_outputs")] <- "Live outputs"
+  initDF$Parameter[(row.names(initDF)=="specific_locations")] <- "Specific Location File"
+  initDF$Parameter[(row.names(initDF)=="save_final_state")] <- "Output Model State Timesteps"
+  initDF$Parameter[(row.names(initDF)=="read_state")] <- "Read State"
+  
+  initFileSplit <- unlist(initFileLookup[match(row.names(initDF),names(initFileLookup))])
+  
+  initDFs <- split(initDF,initFileSplit)
+  
+  if (initDFs$`1`['read_state',]$Value){
+    initDFs$`1`['read_state',]$Value <- "ReadModelState.csv"
+  } else {
+    initDFs$`1`['read_state',]$Value <- ""
+  }
+  
+  if (initDFs$`1`['specific_locations',]$Value){
+    initDFs$`1`['specific_locations',]$Value <- "SpecificLocations.csv"
+  } else {
+    initDFs$`1`['specific_locations',]$Value <- ""
+  }
+  
+  initDFs$`1` <- rbind(initDFs$`1`,data.frame(Parameter=c("Plankton size threshold",
+                                                          "Impact Cell Index",
+                                                          "Dispersal only",
+                                                          "Dispersal only type"),
+                                              Value=c(0.01,
+                                                      "all",
+                                                      "no",
+                                                      "advection")))
+  
+  if (initDFs$`2`['save_final_state',]$Value){
+    initDFs$`2`['save_final_state',]$Value <- (initDFs$`1`['duration',]$Value*12)-1
+  } else {
+    initDFs$`2`['save_final_state',]$Value <- 999999999
+  }
+  
+  initDFs$`2` <- rbind(initDFs$`2`,data.frame(Parameter=c("Track marine specifics",
+                                                          "New Cohorts Filename",
+                                                          "Maturity Filename",
+                                                          "Predation Flows Filename",
+                                                          "Herbivory Flows Filename",
+                                                          "Biomasses Eaten Filename",
+                                                          "Trophic Flows Filename",
+                                                          "Mortality Filename",
+                                                          "Extinction Filename",
+                                                          "Growth Filename",
+                                                          "Metabolism Filename",
+                                                          "NPP Filename"),
+                                              Value=c("no",
+                                                      "NewCohorts",
+                                                      "Maturity",
+                                                      "PredationFlows",
+                                                      "HerbivoryFlows",
+                                                      "BiomassesEaten",
+                                                      "TrophicFlows",
+                                                      "Mortality",
+                                                      "Extinction",
+                                                      "Growth",
+                                                      "Metabolism",
+                                                      "NPP")))
+  
+  initDFs$'3' <- data.frame(Parameter=c("Mass Bin Filename",
+                                        "Environmental Data File",
+                                        "Cohort Functional Group Definitions File",
+                                        "Stock Functional Group Definitions File",
+                                        "Ecological parameters file"),
+                            Value=c("MassBinDefinitions.csv",
+                                    "EnvironmentalDataLayers.csv",
+                                    "CohortFunctionalGroupDefinitions.csv",
+                                    "StockFunctionalGroupDefinitions.csv",
+                                    "EcologicalParameters.csv"))
+  
+  write.csv(x = initDFs$`1`,file = "input/Model setup/SimulationControlParameters.csv",
+            quote = FALSE,row.names = FALSE)
+  write.csv(x = initDFs$`2`,file = "/input/Model setup/OutputControlParameters.csv",
+            quote = FALSE,row.names = FALSE)
+  write.csv(x = initDFs$`3`,file = "/input/Model setup/FileLocationParameters.csv",
+            quote = FALSE,row.names = FALSE)
+  write.table(x = initDFs$`4`$Value,
+              file = "/input/Model setup/Initial Model State Setup/OutputsLabel.csv",
+              sep=",",quote = FALSE,row.names = FALSE,col.names = FALSE)
+  
+  write.csv(x = init$locations,
+            file = "input/Model setup/Initial model state setup/SpecificLocations.csv",
+            quote = FALSE,row.names = FALSE)
   
   shell("Madingley.exe")
  
